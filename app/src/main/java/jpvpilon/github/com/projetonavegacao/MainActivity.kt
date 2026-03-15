96bcc349c18d002d601d19c61bb4380714ac2e60
jpvpilon <jpvelosopilon@gmail.com>
2026-03-15 13:02:44 -0300

    Removendo exemplos e limpando a MainActivity

--- app/src/main/java/jpvpilon/github/com/projetonavegacao/MainActivity.kt
+++ app/src/main/java/jpvpilon/github/com/projetonavegacao/MainActivity.kt
@@ -20,28 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProjetonavegacaoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProjetonavegacaoTheme {
-        Greeting("Android")
     }
 }
